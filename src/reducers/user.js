5c63259105588add9e646e479558babef364bc6f
Chen Wang <cwang138@illinois.edu>
2021-10-20 10:57:01 -0500

    add reducer

---
+++ src/reducers/user.js
@@ -0,0 +1,18 @@
+import {SET_USER, LOGIN_ERROR, LOGOUT} from "../actions/user";
+
+const defaultState = {Authorization: null, loginError: false};
+
+const user = (state = defaultState, action) => {
+	switch(action.type) {
+		case SET_USER:
+			return Object.assign({}, state, {Authorization: action.Authorization, loginError: false});
+		case LOGIN_ERROR:
+			return Object.assign({}, state, {Authorization: null, loginError: true});
+		case LOGOUT:
+			return Object.assign({}, state, {Authorization: null, loginError: false});
+		default:
+			return state;
+	}
+};
+
+export default user;
