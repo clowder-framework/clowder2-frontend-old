b8a5df7b395ab9a92b09ffbda0619d18a23bb14b
Chen Wang <cwang138@illinois.edu>
2021-11-16 17:19:44 -0600

    add different routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import {Route, Switch} from "react-router-dom";
 
-// import App from "./containers/App";
 import {App} from "./components/App";
+import {Dataset} from "./components/Dataset";
+import {File} from "./components/File";
 
 const Routes = (
 	<Switch>
 		<Route exact path="/" render={() => {return (<App/>);}}/>
+		<Route path="/datasets" render={() => {return (<Dataset/>);}}/>
+		<Route path="/files" render={() => {return (<File/>);}}/>
 	</Switch>);
 
 export default Routes;
