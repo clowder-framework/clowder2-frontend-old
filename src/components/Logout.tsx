ef1a2ffd8cb0a7871338d6966b1dadfda692d6cb
Luigi Marini <lmarini@illinois.edu>
2022-01-26 10:50:11 -0600

    Fixed popup MUI version. Fixed logout component.

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,16 +1,15 @@
 import React, {useEffect} from "react";
 import {useDispatch} from "react-redux";
 import {logout as logoutAction} from "../actions/user";
-import {useNavigate} from "react-router-dom";
+import {Navigate} from "react-router-dom";
 
 export const Logout = (): JSX.Element => {
 	const dispatch = useDispatch();
 	const logout = () => dispatch(logoutAction());
-	const history = useNavigate();
 	// component did mount
-	useEffect(() => { logout(); 	history("/login");}, []);
+	useEffect(() => { logout();}, []);
 
 	return (
-		<div/>
+		<Navigate to={"/login"}/>
 	)
 }
