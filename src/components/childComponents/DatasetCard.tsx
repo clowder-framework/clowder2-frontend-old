7e4e8c8232899db352f971e129d6ff7b756cbf27
Luigi Marini <lmarini@illinois.edu>
2022-02-09 10:21:27 -0600

    Removed unused import.

--- src/components/childComponents/DatasetCard.tsx
+++ src/components/childComponents/DatasetCard.tsx
@@ -4,7 +4,6 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import {CardActionArea} from "@mui/material";
 import { parseISO, format } from 'date-fns'
 import {useNavigate} from "react-router-dom";
 
