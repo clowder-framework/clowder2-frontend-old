ff6b1f6d18af68681234bd0c5cdd1635d9a36ae2
Chen Wang <cwang138@illinois.edu>
2022-02-09 17:45:39 -0600

    don't include chip on breadcrumb; doesn't make sense

--- src/components/File.tsx
+++ src/components/File.tsx
@@ -147,9 +147,6 @@ export const File = (): JSX.Element => {
 			<TopBar/>
 			<div className="outer-container">
 				<MainBreadcrumbs paths={paths}/>
-				{/*TODO this should be version number; for now put version ID instead*/}
-				{ fileMetadata !== undefined && fileMetadata.version !== undefined ? <VersionChip
-					versionNumber={fileMetadata.version.slice(0,2)}/> : <></>}
 				{/*Error Message dialogue*/}
 				<ActionModal actionOpen={errorOpen} actionTitle="Something went wrong..." actionText={reason}
 							 actionBtnName="Report" handleActionBtnClick={() => console.log(reason)}
