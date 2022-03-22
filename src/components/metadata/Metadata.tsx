5a3d75ce50954e82579ff6b4694000824af23fb1
Chen Wang <cwang138@illinois.edu>
2022-03-22 13:55:04 -0500

    change how metadata config is written

--- src/components/metadata/Metadata.tsx
+++ src/components/metadata/Metadata.tsx
@@ -2,14 +2,38 @@ import React from "react";
 import {Box, Button} from "@mui/material";
 import metadataConfig from "../../metadata.config";
 
-export const Metadata = () => {
+
+export const Metadata = (props) => {
+	// const {metadataDefinition} = props;
+	const metadataDefinition =
+		[{
+			"widget_id": "",
+			"widget_name": "alternative_title",
+			"key": ""
+		},
+		{
+			"widget_id": "",
+			"widget_name": "doi",
+			"key": "",
+		},
+		{
+			"widget_id": "",
+			"widget_name": "time",
+			"key": "",
+		},
+		{
+			"widget_id": "",
+			"widget_name": "unit",
+			"key": "",
+		}];
+
 	return (
 		<>
 			{
-				metadataConfig.map((item) => {
-					if (item.allowed) {
+				metadataDefinition.map((item) => {
+					if (item.widget_name in metadataConfig) {
 						return (
-							<Box className="inputGroup">{item.widget}</Box>
+							<Box className="inputGroup">{metadataConfig[item.widget_name]}</Box>
 						);
 					}
 				})
--- src/metadata.config.tsx
+++ src/metadata.config.tsx
@@ -4,35 +4,11 @@ import {DOI} from "./components/metadata/DOI";
 import {Time} from "./components/metadata/Time";
 import {Unit} from "./components/metadata/Unit";
 
-const configMetadata = [
-	{
-		"widget_id": "",
-		"widget_name": "alternative_title",
-		"allowed": true,
-		"key": "",
-		"widget": <AlternativeTitle/>
-	},
-	{
-		"widget_id": "",
-		"widget_name": "doi",
-		"allowed": false,
-		"key": "",
-		"widget": <DOI/>
-	},
-	{
-		"widget_id": "",
-		"widget_name": "time",
-		"allowed": true,
-		"key": "",
-		"widget": <Time/>
-	},
-	{
-		"widget_id": "",
-		"widget_name": "unit",
-		"allowed": true,
-		"key": "",
-		"widget": <Unit/>
-	},
-];
+const configMetadata = {
+	"alternative_title": <AlternativeTitle/>,
+	"doi":<DOI/>,
+	"time": <Time/>,
+	"unit": <Unit/>
+};
 
 export default configMetadata
